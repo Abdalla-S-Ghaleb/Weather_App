0c48ce565121ffe43a951def1cba3fad7cdfa5ec
Abdallah Ghaleb <abdallah.s.ghaleb0@gmail.com>
2026-09-19 11:36:43 +0300

    adding security

--- Weather/data.py
+++ Weather/data.py
@@ -1,25 +1,17 @@
 import requests
+from dotenv import find_dotenv, load_dotenv
+import os
 
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
 
-API_key = '3b2461f40913894931f208451ae8e549'
+key = os.getenv("API_KEY")
 
 def get_data(city_name):
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}'
     response = requests.get(url)
 
     if response.status_code != 200:
         return None
 
     return response.json()
-
-
-# def load_icon(icon_code):
-#     url = f"https://openweathermap.org/img/wn/{icon_code}@2x.png"
-#
-#     response = requests.get(url)
-#
-#     if response.status_code != 200:
-#         print("Failed to download icon:", response.status_code)
-#         return None
-#
-#     return image.convert_alpha()
